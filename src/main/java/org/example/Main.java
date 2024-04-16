aee63e8763e8d6c44687b1f6a7a24679ed85d229
Martin Cé <72087021+Guestas@users.noreply.github.com>
2024-04-16 16:11:25 +0200

    newFeauture in new branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,16 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 	public static void main(String[] args) {
-
+		int j = 455;
 		for (int i = 1; i <= 5; i=i+4) {
 			System.out.println("i is: "+i);
+			System.out.println(i);
+			j=j+i;
+		}
+		if (j>455){
+			System.out.println("bigger");
+		} else {
+			System.out.println("smaler");
 		}
 	}
 }
