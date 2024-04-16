4b5318de9b44bc39d497abbef46e529c358aa5e0
Martin Cé <72087021+Guestas@users.noreply.github.com>
2024-04-16 15:36:08 +0200

    comm 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,9 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 	public static void main(String[] args) {
+
 		for (int i = 1; i <= 5; i=i+4) {
-			//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-			// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-			System.out.println("i = " + i);
-			System.out.println("again2");
+			System.out.println("ij = " + i);
 		}
 	}
 }
