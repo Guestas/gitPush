41fd22e29cadb38e0941d1e203bdf183196d1ca3
Martin Cé <72087021+Guestas@users.noreply.github.com>
2024-04-16 17:04:03 +0200

    1703

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,16 @@ public class Main {
 		int j = 455;
 		for (int i = 1; i <= 5; i=i+4) {
 			System.out.println("i is: "+i);
-			i = i-1;
+			i = i-4;
 			System.out.println(i);
 			j=j+i;
 		}
 		if (j>455){
-			System.out.println("bigger");
-			j++;
-			System.out.println(j);
+			System.out.println("bigger number");
+			j--;
+			System.out.println(j--);
 		} else {
-			System.out.println("smaler");
+			System.out.println("smaler number");
 		}
 		System.out.println("program ended here");
 	}
