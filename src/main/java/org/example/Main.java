73658e11b93bcb6aacc5cc60790d42c8a57b2843
Martin Cé <72087021+Guestas@users.noreply.github.com>
2024-04-16 16:01:46 +0200

    new feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 5; i=i+4) {
-			//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-			// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-			System.out.println("i = " + i);
-			System.out.println("again3");
+			System.out.println(i);
 		}
 	}
 }
